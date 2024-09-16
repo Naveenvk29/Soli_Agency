99f00dabcadafa616f2b97c96f5ee282e998fd56
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-09-17 00:35:57 +0530

    added store and userApi & authSlice

---
+++ client/src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react";
+import { apiSlice } from "./Api/apiSlice";
+import authReducer from "./features/authSlice";
+
+const store = configureStore({
+  reducer: { [apiSlice.reducerPath]: apiSlice.reducer, auth: authReducer },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(),
+  devTools: true,
+});
+setupListeners(store.dispatch);
+export default store;
