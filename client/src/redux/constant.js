efc37756b94d17e3b8f169b9ce6fea2bbc874a84
Naveenvk29 <naveenvinodkumar29@gmail.com>
2024-10-21 19:10:35 +0530

    add api

--- client/src/redux/constant.js
+++ client/src/redux/constant.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "";
+export const BASE_URL = "https://soil-agency-api.onrender.com";
 export const USERS_URL = "/api/users";
 export const SOILS_URL = "/api/soils";
 export const DISTRIBUTORS_URL = "/api/distributors";
